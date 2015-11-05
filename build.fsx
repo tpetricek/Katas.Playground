363b87016e7b9e1759170b9a5fabebbec173911a
Tomas Petricek <tomas@tomasp.net>
2015-11-05 15:49:05 -0800

    Fix wwwroot path

--- build.fsx
+++ build.fsx
@@ -83,7 +83,7 @@ Target "Build" ignore
 
 Target "Deploy" (fun _ ->
   let sourceDirectory = __SOURCE_DIRECTORY__
-  let wwwrootDirectory = __SOURCE_DIRECTORY__ @@ "../../wwwroot"
+  let wwwrootDirectory = __SOURCE_DIRECTORY__ @@ "../wwwroot"
   CleanDir wwwrootDirectory
   CopyRecursive sourceDirectory wwwrootDirectory false |> ignore
 (*
